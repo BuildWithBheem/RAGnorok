bce8f353ac15379f68df37f730227b55bec45627
BuildWithBheem <bskoundinya@gmail.com>
2026-07-16 04:16:51 +0530

    Added Authentication

---
+++ Authenticate.py
@@ -0,0 +1,33 @@
+import mysql.connector
+from fastapi import HTTPException,Header
+pswd = "bheem@3052006"
+connection = mysql.connector.connect(
+host = 'localhost',
+    user = 'root',
+    password = pswd,
+    database = 'rag_db'
+)
+
+sql_comm = connection.cursor()
+
+import secrets   # Module for secure generation of passkeys
+
+def api_key_generator(username):
+    api_key  = secrets.token_hex()
+
+    sql_comm.execute("insert into users (username,api_key) values (%s,%s) ",(username,api_key))
+
+    connection.commit()
+
+    return api_key
+
+def get_user(user_api: str = Header(...)):
+
+    sql_comm.execute("select id from users where api_key = %s",(user_api,))
+
+    key = sql_comm.fetchone()
+
+    if not key:
+        return None
+    
+    return key[0]
